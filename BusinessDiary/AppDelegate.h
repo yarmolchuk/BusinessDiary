94b3cd65aa263057f3f6238bc5a32d784733b024
Dima Yarmolchuk <yarmolchuk@gmail.com>
2014-09-08 20:56:23 +0300

    Initial Commit

---
+++ BusinessDiary/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  BusinessDiary
+//
+//  Created by Dima Yarmolchuk on 08.09.14.
+//  Copyright (c) 2014 yarmolchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ BusinessDiary/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BusinessDiary
+//
+//  Created by Dima Yarmolchuk on 08.09.14.
+//  Copyright (c) 2014 yarmolchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
